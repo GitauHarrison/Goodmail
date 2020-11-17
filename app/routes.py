b7ec92363d5c66fa7adddf2d804b81abbc638f29
Gitau Harrison <harrisongitau3@gmail.com>
2020-11-17 19:37:09 +0300

    Fix footer and display flash message

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,7 @@ from flask import render_template, url_for, flash, redirect
 @app.route('/')
 @app.route('/home')
 def home():
+    flash('This is a test message')
     return render_template('home.html', title = 'Home')
 
 @app.route('/about')
