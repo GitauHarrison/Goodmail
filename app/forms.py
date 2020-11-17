c292cedd9c256fe2033c2df009fbbd2ced5a93c4
Gitau Harrison <harrisongitau3@gmail.com>
2020-11-17 20:22:04 +0300

    Add form validation

--- app/forms.py
+++ app/forms.py
@@ -3,6 +3,6 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Email
 
 class HomeForm(FlaskForm):
-    email = StringField('Email', render_kw={"placeholder": "Your Email"})
-    site = StringField('Site', render_kw={"placeholder": "Site URL"})
+    email = StringField('Email', validators=[DataRequired(), Email()], render_kw={"placeholder": "Your Email"})
+    site = StringField('Site', validators=[DataRequired()], render_kw={"placeholder": "Site URL"})
     submit = SubmitField('Submit')
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,16 @@
-from app import app
+from app import app, db
 from flask import render_template, url_for, flash, redirect
 from app.forms import HomeForm
 
 @app.route('/')
-@app.route('/home')
+@app.route('/home', methods = ['GET', 'POST'])
 def home():
     form = HomeForm()
-    flash('This is a test message')
+    if form.validate_on_submit():
+        # user = User(email = form.email.data, site = form.site.data)
+        # db.session.add(user)
+        # db.session.commit()
+        flash('Your data was saved successfully!')
     return render_template('home.html', title = 'Home', form = form)
 
 @app.route('/about')
