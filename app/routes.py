5ce48b2c6529a0cbd438dfde5c0ba57ee182993e
Gitau Harrison <harrisongitau3@gmail.com>
2020-11-17 20:59:07 +0300

    Update user data in home page and User model

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,16 @@
 from app import app, db
 from flask import render_template, url_for, flash, redirect
 from app.forms import HomeForm
+from app.models import User
 
 @app.route('/')
 @app.route('/home', methods = ['GET', 'POST'])
 def home():
     form = HomeForm()
     if form.validate_on_submit():
-        # user = User(email = form.email.data, site = form.site.data)
-        # db.session.add(user)
-        # db.session.commit()
+        user = User(username = form.username.data, email = form.email.data, site = form.site.data)
+        db.session.add(user)
+        db.session.commit()
         flash('Your data was saved successfully!')
     return render_template('home.html', title = 'Home', form = form)
 
